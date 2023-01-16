d4cacb627235761b72939ea9dba93f571fefcf52
comar <saywooong@gmail.com>
2023-01-16 10:46:33 +0900

    feat: work in local

---
+++ mk_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda env create -f environment.yaml
+conda activate ldm
+pip install packaging==21.3
+pip install 'torchmetrics<0.8'
+pip install kornia==0.5
+pip install academictorrents # confict occurs, but work
